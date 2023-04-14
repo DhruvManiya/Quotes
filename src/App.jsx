84542489486a8e9892e470402d59455c17eba557
Dhruv Maniya <81362979+DhruvManiya@users.noreply.github.com>
2023-04-14 20:00:40 +0530

    navbar with routing done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import Navbar from './UI/Navbar'
+import React from "react";
+import Navbar from "./UI/Navbar";
+import { Route, Routes, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import Bookmarks from "./pages/Bookmarks";
 
 const App = () => {
   return (
-    <Navbar />
-  )
-}
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/bookmarks" element={<Bookmarks />} />
+      </Routes>
+    </>
+  );
+};
 
-export default App
+export default App;
--- src/UI/Navbar.jsx
+++ src/UI/Navbar.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <>
-    </>
-  )
-}
+      <nav className="h-20 w-full flex justify-between items-center text-[1.75rem] bg-[#333333] shadow-lg font-[Poppins]">
+        <h1 className="ml-20 font-[600] tracking-wide text-white">
+          <Link to="/home">HOME</Link>
+        </h1>
 
-export default Navbar
+        <h1 className="mr-20 font-[600] tracking-wide text-[#F3CB89]">
+          {" "}
+          <Link to="/bookmarks">BOOKMARKS</Link>
+        </h1>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
